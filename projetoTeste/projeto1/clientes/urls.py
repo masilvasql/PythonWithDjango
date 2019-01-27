8682766eee1aad4c356ba562e229b2a0181b910c
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-01-27 21:28:42 -0200

    Create, Update, List do crud

--- projetoTeste/projeto1/clientes/urls.py
+++ projetoTeste/projeto1/clientes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import personList
-
+from .views import personNew
+from .views import personUpdate
 
 urlpatterns = [
-    path('list/', personList),
+    path('list/', personList, name = 'personList'),
+    path('new/', personNew, name = 'personNew'),
+    path('update/<int:id>/', personUpdate, name ='personUpdate')
 ]
--- projetoTeste/projeto1/clientes/views.py
+++ projetoTeste/projeto1/clientes/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import person
-
+from .forms import PersonForm
 
 def personList(request):
     persons = person.objects.all()
     return render(request, 'person.html', { 'persons' : persons } )
+
+def personNew(request):
+    form = PersonForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('personList')
+    return render(request, 'person_form.html', {'form': form})
+
+def personUpdate(request, id):
+    p = get_object_or_404(person, pk = id)
+    form = PersonForm(request.POST or None,  request.FILES or None, instance = p)
+    if form.is_valid():
+        form.save()
+        return redirect('personList')
+    return render(request, 'person_form.html', {'form':form})
